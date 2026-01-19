a6c63099c4103642ac087d87fb5339eca1f10f64
Stefan Steininger <stefan.steininger@frauscher.com>
2026-01-19 19:54:20 +0100

    Chore: Fix windows build

--- src/lib.rs
+++ src/lib.rs
@@ -79,6 +79,7 @@ pub use format::{Formatter3164, Formatter5424, LogFormat};
 
 pub type Priority = u8;
 
+#[cfg(unix)]
 const UNIX_SOCK_PATHS: [&str; 3] = ["/dev/log", "/var/run/syslog", "/var/run/log"];
 
 /// Main logging structure
@@ -261,7 +262,10 @@ pub fn unix<F: Clone>(formatter: F) -> Result<Logger<LoggerBackend, F>> {
 
 #[cfg(not(unix))]
 pub fn unix<F: Clone>(_formatter: F) -> Result<Logger<LoggerBackend, F>> {
-    Err(ErrorKind::UnsupportedPlatform)?
+    Err(Error::Io(io::Error::new(
+        io::ErrorKind::Unsupported,
+        "unsupported platform",
+    )))?
 }
 
 /// Returns a Logger using unix socket to target local syslog at user provided path
@@ -272,7 +276,10 @@ pub fn unix_custom<P: AsRef<Path>, F>(formatter: F, path: P) -> Result<Logger<Lo
 
 #[cfg(not(unix))]
 pub fn unix_custom<P: AsRef<Path>, F>(_formatter: F, _path: P) -> Result<Logger<LoggerBackend, F>> {
-    Err(ErrorKind::UnsupportedPlatform)?
+    Err(Error::Io(io::Error::new(
+        io::ErrorKind::Unsupported,
+        "unsupported platform",
+    )))?
 }
 
 #[cfg(unix)]
@@ -387,7 +394,10 @@ pub fn init_unix(facility: Facility, log_level: log::LevelFilter) -> Result<()> 
 
 #[cfg(not(unix))]
 pub fn init_unix(_facility: Facility, _log_level: log::LevelFilter) -> Result<()> {
-    Err(ErrorKind::UnsupportedPlatform)?
+    Err(Error::Io(io::Error::new(
+        io::ErrorKind::Unsupported,
+        "unsupported platform",
+    )))?
 }
 
 /// Unix socket Logger init function compatible with log crate and user provided socket path
@@ -419,7 +429,10 @@ pub fn init_unix_custom<P: AsRef<Path>>(
     _log_level: log::LevelFilter,
     _path: P,
 ) -> Result<()> {
-    Err(ErrorKind::UnsupportedPlatform)?
+    Err(Error::Io(io::Error::new(
+        io::ErrorKind::Unsupported,
+        "unsupported platform",
+    )))?
 }
 
 /// UDP Logger init function compatible with log crate
